316f1a851dfbdbe38c89485ba87e2c63b9aa175a
gautam777777 <gautamsharma0057@gmail.com>
2023-03-03 14:48:15 +0530

    pro9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as GrandParent , Routes as Parent , Route as Child} from 'react-router-dom'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import NotFound from './Pages/NotFound'
+import Register from './Pages/Register'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <GrandParent>
+        <Parent>
+            <Child path="/" element={<Home />}></Child>
+            <Child path="/login" element={<Login />}></Child>
+            <Child path="/register" element={<Register />}></Child>
+            <Child path="*" element={<NotFound />}></Child>
+        </Parent>
+    </GrandParent>
+  )
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+//1.Import Area
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//2.Defination Area
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render( <App /> )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//3.Export Area
+
+
+//3.1 Default export
+
+
+//3.2 Named export
+  
